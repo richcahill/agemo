a71fcb94460145917e2780965cd09e6b995d8a43
Rich Cahill <rich@rc3.me>
2023-08-21 22:33:13 +0100

    begin step layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,17 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between`}>
+    <main className={`flex min-h-screen flex-col items-center gap-4`}>
       <Head>
         <title>Agemo</title>
         <link rel='icon' href='/favicon.png' />
       </Head>
       <Nav />
-      <div className='flex-1 p-4 w-full'></div>
+      <div className='flex-1 p-4 pb-0 w-full h-64 bg-white/5 relative overflow-scroll'>
+        <div className='w-full h-full bg-white/5 p-4 rounded-t-3xl'>
+          <div className='text-sm uppercase tracking-wide'>SCOPE</div>
+        </div>
+      </div>
     </main>
   );
 }
